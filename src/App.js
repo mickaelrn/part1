a538ed5ce143c42cf67b411644ac356a89da458d
mickaelrn <ravimananaelisoa@gmail.com>
2022-10-05 15:45:36 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Header = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+const Content  = (props) => {
+  return (
+    <>
+      <p>{props.partname} {props.exercices}</p>
+    </>
+  )
+}
+const Total  = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.total}</p>
+    </>
+  )
 }
 
-export default App;
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content partname={part1} exercices={exercises1}/>
+      <Content partname={part2} exercices={exercises2}/>
+      <Content partname={part3} exercices={exercises3}/>
+      <Total total={exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+export default App
